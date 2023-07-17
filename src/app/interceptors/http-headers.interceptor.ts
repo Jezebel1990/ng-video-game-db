8f1d2d59640539fb0fb7ec8c0f9250dee5c71745
Jezebel1990 <jezebelguedes@gmail.com>
2023-07-17 00:43:10 -0300

    update key

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -16,7 +16,7 @@ export class HttpHeadersInterceptor implements HttpInterceptor{
                     'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
             },
             setParams: {
-                key: 'e40e743af2c94b0c916a8aa618fb4473',
+                key: '4bb80b2e6d2446638fe1568c6449f432',
             }
             
         });
